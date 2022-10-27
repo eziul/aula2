7432665997a4cf31d3e77366d8ef6f9e3e18f09d
Cleber <cleber@ecocentauro.com.br>
2022-10-27 18:37:08 -0300

    Commit aula2

--- main.ts
+++ main.ts
@@ -1,11 +1,22 @@
 import * as restify from 'restify'
+import { UsuarioRepository } from './usuarios/usuarios'
 
 const server = restify.createServer()
 
+server.use(restify.plugins.queryParser())
+server.use(restify.plugins.bodyParser())
 
 server.get("/hello", (req, resp, next) => {
     resp.send(200, "Hello World")
 })
+
+
+
+var usuarioRep = new UsuarioRepository()
+
+
+server.post("/usuario", usuarioRep.criarUsuario)
+server.get("/usuarios", usuarioRep.todosUsuarios)
 
 server.listen(3000, ()=> {
     console.log("Server is on-line")
---
+++ usuarios/usuarios.ts
@@ -0,0 +1,55 @@
+import * as sqlite from 'sqlite3'
+import * as restify from 'restify'
+
+
+export class Usuario {
+    id: number
+    nome: string
+    login: string
+    senha: string
+}
+
+export class UsuarioRepository {
+
+    db: sqlite.Database
+
+
+    constructor () {
+        this.db = new sqlite.Database('./apidb.db')
+        this.db.run("CREATE TABLE if NOT EXISTS usuario ( id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, login TEXT, senha TEXT)")
+    }
+
+    criarUsuario = (req: restify.Request, resp: restify.Response, next: restify.Next) => {
+        this.db.run('INSERT INTO usuario (nome, login, senha) VALUES (?, ?, ?);', req.body.nome, req.body.login, req.body.senha, (error) => {
+            if (error) {
+                resp.send(500)
+            } else {
+                resp.send(204)
+            }
+        })
+    }
+
+    todosUsuarios = (req: restify.Request, resp: restify.Response, next: restify.Next) => {
+        var usuarios: Usuario[] = []
+        this.db.all("select * from usuario", (error, rows) => {
+           
+
+            if (error) {
+                resp.send(500)
+            } else {
+                rows.forEach(item => {
+                    var u = new Usuario()
+                    u.id = item.id
+                    u.login = item.login
+                    u.senha = item.senha
+                    usuarios.push(u)
+                })                    
+                resp.send(usuarios)
+                
+            }
+
+        })
+
+
+    }
+}
